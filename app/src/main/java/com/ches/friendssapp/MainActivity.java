e79ee0cffd0441b28cd3ca3e71d2ba4f82ada91c
chesna-j <chesnajohnson11a@gmail.com>
2024-06-29 15:12:50 +0530

    done

--- app/src/main/java/com/ches/friendssapp/MainActivity.java
+++ app/src/main/java/com/ches/friendssapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ches.friendssapp;
 
 import android.annotation.SuppressLint;
+import android.app.DownloadManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -13,10 +14,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
      EditText e1,e2,e3,e4;
      AppCompatButton b1;
     @SuppressLint("MissingInflatedId")
+    String apiurl="https://friendsapi-re5a.onrender.com/adddata";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,7 +46,41 @@ public class MainActivity extends AppCompatActivity {
                 String getFname=e2.getText().toString();
                 String getNickame=e3.getText().toString();
                 String getDescribe=e4.getText().toString();
-                Toast.makeText(getApplicationContext(),getName+getFname+getNickame+getDescribe,Toast.LENGTH_SHORT).show();
+
+                //JSON object creation
+
+                JSONObject friend =new JSONObject();
+                try {
+                    friend.put("name",getName);
+                    friend.put("friendName", getFname);
+                    friend.put("friendNickName", getNickame);
+                    friend.put("DescribeYourFriend",  getDescribe);
+                    //json request
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+                JsonObjectRequest jsonObjectRequest =new JsonObjectRequest(
+                        Request.Method.POST,
+                        apiurl,
+                        friend,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Toast.makeText(getApplicationContext(), "submitted", Toast.LENGTH_SHORT).show();
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(getApplicationContext(), "Something went wrong", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                );
+                RequestQueue requestQueue= Volley.newRequestQueue(getApplicationContext());
+                requestQueue.add(jsonObjectRequest);
+
+                //Toast.makeText(getApplicationContext(),getName+getFname+getNickame+getDescribe,Toast.LENGTH_SHORT).show();
 
             }
         });
